a8a1a1b8cb8f00e52cb91aff9021155108d8c485
Subhaan Jirait <101044609+Subhaansj7@users.noreply.github.com>
2024-07-14 19:39:08 +0530

    Update 3.Second Largest_second smallest in array(BRUTE with sort).py

--- Array/Questions/3.Second Largest_second smallest in array(BRUTE with sort).py
+++ Array/Questions/3.Second Largest_second smallest in array(BRUTE with sort).py
@@ -1,4 +1,7 @@
-
+#Approach:
+#Sort the array in ascending order
+#The element present at the second index is the second smallest element
+#The element present at the second index from the end is the second largest element
 def getElements(arr, n):
     if n == 0 or n == 1:
         print(-1, -1)  # edge case when only one element is present in array
