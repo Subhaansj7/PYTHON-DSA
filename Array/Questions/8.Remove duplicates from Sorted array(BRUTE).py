91c577031ec9cfb5ad7f54d71d05ad62bd20064c
Subhaan Jirait <101044609+Subhaansj7@users.noreply.github.com>
2024-07-14 19:38:18 +0530

    Update 8.Remove duplicates from Sorted array(BRUTE).py

--- Array/Questions/8.Remove duplicates from Sorted array(BRUTE).py
+++ Array/Questions/8.Remove duplicates from Sorted array(BRUTE).py
@@ -1,4 +1,11 @@
+#Approach: 
 
+#Declare a HashSet.
+#Run a for loop from starting to the end.
+#Put every element of the array in the set.
+#Store size of the set in a variable K.
+#Now put all elements of the set in the array from the starting of the array.
+#Return K.
 def removeDuplicates(arr):
     st=set()
     for i in range(len(arr)):
